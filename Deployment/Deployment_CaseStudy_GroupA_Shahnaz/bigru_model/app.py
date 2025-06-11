546b0d4f98031a1263fe6f2dcb2658b34ffd2e65
shahnazpm <62134027+shahnazpm@users.noreply.github.com>
2025-06-11 18:29:54 -0400

    Update sonarqube error correction

--- Deployment/Deployment_CaseStudy_GroupA_Shahnaz/bigru_model/app.py
+++ Deployment/Deployment_CaseStudy_GroupA_Shahnaz/bigru_model/app.py
@@ -3,25 +3,17 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 import yaml
-import os
 
 # Load config
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
 app = Flask(__name__)
-word_lenth=10
 
 model = load_model("BiGRU_model.h5")
 vocab_size = config["model"]["vocab_size"]
 maxlen = config["model"]["maxlen"]
 port = config["server"]["port"]
-def predict():
-    data = request.get_json()
-    sequences = data.get("sequences")  # list of tokenized sequences
-    sequences = pad_sequences(sequences, maxlen=maxlen)
-    preds = model.predict(sequences)
-    return jsonify({"predictions": preds.tolist()})
 
 @app.route("/predict", methods=["POST"])
 def predict():
